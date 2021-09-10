03b5375e98194400c2331acdc41a950b116bee18
Jean David <jeandavid54@aol.com>
2021-09-10 12:10:52 +0200

    Add files via upload

--- squircle.py
+++ squircle.py
@@ -2,14 +2,18 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 
-x = np.linspace(-1,1,100)
+# center of squircle
+a,b = -0,1.5
+# little radius of squircle
+r = 3
+x = np.linspace(-r+a,r+a,100)
 x_value = list(x) + list(reversed(list(x)))
 
 def squircle(n):
-    # x**n + y**n = 1
-    s1 = [(abs(v)**n) for v in x]
-    s2 = [((1 - abs(v))**(1/n)) for v in s1]
-    s3 = s2 + [-v for v in s2]        
+    # x**n + y**n = r**n
+    s1 = [(abs(v-a)**n) for v in x]
+    s2 = [abs((r**n - abs(v))**(1/n)+b) for v in s1]
+    s3 = s2 + [2*b-v for v in s2]        
     plt.plot(x_value, s3)
     plt.axis('equal')
     #plt.show()
